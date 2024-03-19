9fc73c354c39c4e92f0fdaead80ab4a4b56054de
Joaoopeedro <joaopedronr37@gmail.com>
2024-03-19 13:13:09 -0300

    :boom: fix: Ajustes feitos

--- educarte/lib/Interector/validations/convertter.dart
+++ educarte/lib/Interector/validations/convertter.dart
@@ -1,11 +1,13 @@
 import 'package:intl/intl.dart';
 
 class Convertter {
-  static Future<List<String>> getCurrentDate() async{
+  static Future<List<String>> getCurrentDate({bool? isDe, String? data}) async{
     DateTime now = DateTime.now();
     String locale = 'pt_BR';
-
-    DateFormat startFormat = DateFormat('EEEE, dd', locale);
+    if(data != null){
+      now = DateTime.parse(data);
+    }
+    DateFormat startFormat = DateFormat(isDe != null ? 'dd' :'EEEE, dd', locale);
     String start = startFormat.format(now);
       
     start = start.replaceRange(0, 1, start[0].toUpperCase());
@@ -13,6 +15,6 @@ class Convertter {
     String center = DateFormat(' MMMM', locale).format(now).toUpperCase();
     String end = DateFormat('yyyy', locale).format(now);
 
-    return ["$start de", center, " de $end"];
+    return ["$start de", center, isDe != null ? end : " de $end"];
   }
 }
