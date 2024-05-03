886b74d118b09b2403e910c2f149930e37adfa67
Joaoopeedro <joaopedronr37@gmail.com>
2024-05-03 12:54:06 -0300

    Adjustments made by the application

---
+++ educarte/lib/Interector/models/entry_and_exit_modal.dart
@@ -0,0 +1,85 @@
+class EntryAndExit {
+  String? date;
+  List<AccessControls>? accessControls;
+  ContractedHour? contractedHour;
+
+  EntryAndExit(
+      {this.date, this.accessControls, this.contractedHour});
+
+  EntryAndExit.fromJson(Map<String, dynamic> json) {
+    date = json['date'];
+    if (json['accessControls'] != null) {
+      accessControls = <AccessControls>[];
+      json['accessControls'].forEach((v) {
+        accessControls!.add(AccessControls.fromJson(v));
+      });
+    }
+    contractedHour = json['contractedHour'] != null
+        ? ContractedHour.fromJson(json['contractedHour'])
+        : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    data['date'] = this.date;
+    if (this.accessControls != null) {
+      data['accessControls'] =
+          this.accessControls!.map((v) => v.toJson()).toList();
+    }
+    if (this.contractedHour != null) {
+      data['contractedHour'] = this.contractedHour!.toJson();
+    }
+    return data;
+  }
+}
+
+class AccessControls {
+  String? id;
+  int? accessControlType;
+  String? time;
+
+  AccessControls({this.id, this.accessControlType, this.time});
+
+  AccessControls.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    accessControlType = json['accessControlType'];
+    time = json['time'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    data['id'] = this.id;
+    data['accessControlType'] = this.accessControlType;
+    data['time'] = this.time;
+    return data;
+  }
+}
+
+class ContractedHour {
+  String? id;
+  int? hours;
+  int? status;
+  String? startDate;
+  String? endDate;
+
+  ContractedHour(
+      {this.id, this.hours, this.status, this.startDate, this.endDate});
+
+  ContractedHour.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    hours = json['hours'];
+    status = json['status'];
+    startDate = json['startDate'];
+    endDate = json['endDate'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    data['id'] = this.id;
+    data['hours'] = this.hours;
+    data['status'] = this.status;
+    data['startDate'] = this.startDate;
+    data['endDate'] = this.endDate;
+    return data;
+  }
+}
--- educarte/lib/Ui/screens/entryAndExit/entry_and_exit_page.dart
+++ educarte/lib/Ui/screens/entryAndExit/entry_and_exit_page.dart
@@ -1,10 +1,17 @@
+import 'dart:convert';
+
 import 'package:educarte/Interector/base/constants.dart';
+import 'package:educarte/Interector/models/entry_and_exit_modal.dart';
+import 'package:educarte/Services/config/api_config.dart';
 import 'package:educarte/Ui/components/table_calendar.dart';
 import 'package:educarte/Ui/screens/entryAndExit/widgets/card_entry_and_exit.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import '../../global/global.dart';
 import '../Messagens/messages_screen.dart';
+import 'package:http/http.dart' as http;
+import '../../global/global.dart' as globals;
 
 class EntryAndExitPage extends StatefulWidget {
   const EntryAndExitPage({super.key});
@@ -13,8 +20,10 @@ class EntryAndExitPage extends StatefulWidget {
   State<EntryAndExitPage> createState() => _EntryAndExitPageState();
 }
 
+enum Loadings {none,list}
 class _EntryAndExitPageState extends State<EntryAndExitPage> {
   Loadings loading = Loadings.none;
+  List<EntryAndExit> listAccess = [];
 
   void setLoading({required Loadings load}){
     setState(() {
@@ -22,65 +31,93 @@ class _EntryAndExitPageState extends State<EntryAndExitPage> {
     });
   }
 
+  Future<void> getAccessControls(DateTime startDate, DateTime? endDate)async{
+    setLoading(load: Loadings.list);
+    var params = {
+      'StudentId': idStudent,
+      "StartDate": startDate.toString(),
+      "EndDate": endDate == null ? startDate.toString() : endDate.toString()
+    };
+    var response = await http.get(Uri.parse("$baseUrl/Students/AccessControls/${globals.idStudent}").replace(queryParameters: params),
+      headers: {
+        "Authorization": "Bearer ${globals.token}"
+      },
+    );
+    if(response.statusCode == 200){
+      var decodeJson = jsonDecode(response.body);
+      if(decodeJson["accessControlsByDate"] != null) {
+        decodeJson.forEach((item) => listAccess.add(item));
+      }
+    }
+    setLoading(load: Loadings.none);
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getAccessControls(DateTime.now(),DateTime.now());
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        width: screenWidth(context),
-        height: screenHeight(context),
-        color: colorScheme(context).background,
-        alignment: Alignment.center,
-        child: SafeArea(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: CustomTableCalendar(
-                  paddingTop: 24,
-                  callback:(start, end) {
+      return Scaffold(
+        body: Container(
+          width: screenWidth(context),
+          height: screenHeight(context),
+          color: colorScheme(context).background,
+          alignment: Alignment.center,
+          child: SafeArea(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: CustomTableCalendar(
+                      paddingTop: 24,
+                      callback: (start, end) {
 
-                  }
+                      }
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16),
-                child: Container(
-                  alignment: Alignment.center,
-                  height: 38,
-                  color: colorScheme(context).primary.withOpacity(0.5),
-                  child: Text(
-                    "Saldo de horas: +00h. 00min ",
-                    style: GoogleFonts.poppins(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w500,
-                      color: colorScheme(context).onPrimary
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 38,
+                    color: colorScheme(context).primary.withOpacity(0.5),
+                    child: Text(
+                      "Saldo de horas: +00h. 00min ",
+                      style: GoogleFonts.poppins(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: colorScheme(context).onPrimary
+                      ),
                     ),
                   ),
                 ),
-              ),
-              if(loading == Loadings.list)
-                const Expanded(
-                  child: Center(
-                      child: CircularProgressIndicator()),
-                )
-              else
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8),
-                  child: ListView.builder(
-                    padding: const EdgeInsets.only(top: 10),
-                    shrinkWrap: true,
-                    itemCount: 10,
-                    itemBuilder: (BuildContext context, int index) {
-                      return CardEntryAndExit();
-                    },
-                  ),
-                ),
-              )
-            ],
+                if(loading == Loadings.list)
+                  const Expanded(
+                    child: Center(
+                        child: CircularProgressIndicator()),
+                  )
+                else
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8),
+                      child: ListView.builder(
+                        padding: const EdgeInsets.only(top: 10),
+                        shrinkWrap: true,
+                        itemCount: 10,
+                        itemBuilder: (BuildContext context, int index) {
+                          return CardEntryAndExit();
+                        },
+                      ),
+                    ),
+                  )
+              ],
+            ),
           ),
         ),
-      ),
-    );
+      );
   }
 }
