8eefac266cf4a33abad8482c0f848c191c188df6
Gustavo Borges de Souza <gsouza@sp.senai.br>
2024-07-04 12:58:49 -0300

    ci: rename folder to make pipeline work

--- lib/Ui/routes.dart
+++ lib/Ui/routes.dart
@@ -10,7 +10,7 @@ import 'package:educarte/Ui/screens/auth/recover_password/forgot_password_screen
 import 'package:educarte/Ui/screens/entryAndExit/entry_and_exit_page.dart';
 import 'package:educarte/Ui/screens/home/home_screen.dart';
 import 'package:educarte/Ui/screens/auth/login_screen.dart';
-import 'package:educarte/Ui/screens/messagens/messages_screen.dart';
+import 'package:educarte/Ui/screens/messages/messages_screen.dart';
 import 'package:educarte/Ui/screens/auth/recover_password/redefine_password_screen.dart';
 import 'package:educarte/Ui/screens/time_control/time_control_page.dart';
 import 'package:educarte/Ui/shell/educarte_shell.dart';
@@ -20,42 +20,43 @@ import 'package:jwt_decoder/jwt_decoder.dart';
 
 import 'styles/transitions/fade_transition.dart';
 
-
 class Routes {
   static final GoRouter router = GoRouter(
     debugLogDiagnostics: true,
     initialLocation: '/login',
     // initialLocation: '/login',
-    redirect: (context, state) async{
+    redirect: (context, state) async {
       PersistenceRepository persistenceRepository = PersistenceRepository();
 
       String? path;
       globals.token = await persistenceRepository.read(key: SecureKey.token);
 
-      if(globals.token != null){
+      if (globals.token != null) {
         try {
-          var response = await http.post(Uri.parse("$baseUrl/Auth/refresh"),
-            headers: {
-              "Content-Type": "application/json",
-              "Authorization": "Bearer ${globals.token}",
-            }
-          );
+          var response =
+              await http.post(Uri.parse("$baseUrl/Auth/refresh"), headers: {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer ${globals.token}",
+          });
 
-          if(response.statusCode == 200){
-            await persistenceRepository.update(key: SecureKey.token, value: jsonDecode(response.body)["token"]);
-            Map<String, dynamic> decodedToken = JwtDecoder.decode(jsonDecode(response.body)["token"]);
+          if (response.statusCode == 200) {
+            await persistenceRepository.update(
+                key: SecureKey.token,
+                value: jsonDecode(response.body)["token"]);
+            Map<String, dynamic> decodedToken =
+                JwtDecoder.decode(jsonDecode(response.body)["token"]);
             globals.token = jsonDecode(response.body)["token"];
             globals.id = decodedToken["sub"];
             globals.checkUserType(profileType: decodedToken["profile"]);
-            bool firstAccess = bool.parse(decodedToken["isFirstAccess"].toLowerCase());
+            bool firstAccess =
+                bool.parse(decodedToken["isFirstAccess"].toLowerCase());
             globals.firstAccess = firstAccess;
 
-            if(globals.nome == null){
+            if (globals.nome == null) {
               // currentIndex = 2;
               path = globals.routerPath(firstAccess: firstAccess);
-              
             }
-          }else if(response.statusCode == 401){
+          } else if (response.statusCode == 401) {
             await persistenceRepository.delete(key: SecureKey.token);
             globals.token = null;
             globals.firstAccess = false;
@@ -97,9 +98,7 @@ class Routes {
       GoRoute(
         path: "/searchByVoice",
         builder: (context, state) => SearchByVoicePage(
-          controller: (state.extra as Map)["controller"], 
-          context: context
-        ),
+            controller: (state.extra as Map)["controller"], context: context),
       ),
       ShellRoute(
         builder: (context, state, child) {
@@ -114,14 +113,12 @@ class Routes {
           GoRoute(
             path: "/recados",
             pageBuilder: (context, state) =>
-              FadeTransitionPage(child: const MessagesScreen()
-            ),
+                FadeTransitionPage(child: const MessagesScreen()),
           ),
           GoRoute(
             path: "/entryAndExit",
             pageBuilder: (context, state) =>
-              FadeTransitionPage(child: const EntryAndExitPage()
-            ),
+                FadeTransitionPage(child: const EntryAndExitPage()),
           )
         ],
       ),
